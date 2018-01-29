27caaa2c3df8917ac9862e8f5918fb2893ca5268
cikatuna <31114203+cikatuna@users.noreply.github.com>
2018-01-29 19:08:23 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/domo/cryptotrace/MainActivity.java
+++ app/src/main/java/com/example/domo/cryptotrace/MainActivity.java
@@ -245,9 +245,9 @@ private List<Product> listing;
     }
 
     //Metoda za crashanje aplikacije - test CRASHLYTICS plugina od FABRICA+button u activity_main
-    public void forceCrash(View view) {
-        throw new RuntimeException("This is a crash");
-    }
+   // public void forceCrash(View view) {
+    //    throw new RuntimeException("This is a crash");
+   // }
 
 
 
